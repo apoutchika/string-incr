b5d02d596a4b3d19c768531f25abc95b15f88505
Julien Philippon <juphilippon@gmail.com>
2022-12-16 07:33:24 +0100

    Rename tests

--- index.spec.ts
+++ index.spec.ts
@@ -1,17 +1,17 @@
 import incrString from "./index";
 
 describe("Test increment string", () => {
-  it("Return next numbers", () => {
+  it("should return next numbers", () => {
     expect(incrString("hello 42")).toBe("hello 43");
     expect(incrString("hello world 42")).toBe("hello world 43");
   });
 
-  it("Return first number without separator", () => {
+  it("should return first number without separator", () => {
     expect(incrString("hello")).toBe("hello 2");
     expect(incrString("hello world")).toBe("hello world 2");
   });
 
-  it("Return first number with separator", () => {
+  it("should return first number with separator", () => {
     expect(incrString("hello", "-1")).toBe("hello-1");
     expect(incrString("hello", "-2")).toBe("hello-2");
     expect(incrString("hello", "-42")).toBe("hello-42");
@@ -20,35 +20,35 @@ describe("Test increment string", () => {
     expect(incrString("hello", 2)).toBe("hello 2");
   });
 
-  it("Change last number with space", () => {
+  it("should change last number with space", () => {
     expect(incrString("toto 2")).toBe("toto 3");
     expect(incrString("toto 9")).toBe("toto 10");
     expect(incrString("toto 10")).toBe("toto 11");
   });
 
-  it("Change last number without space", () => {
+  it("should change last number without space", () => {
     expect(incrString("toto2")).toBe("toto3");
     expect(incrString("toto9")).toBe("toto10");
     expect(incrString("toto10")).toBe("toto11");
   });
 
-  it("Like many numbers on name", () => {
+  it("should like many numbers on name", () => {
     expect(incrString("ligne 2 place 1")).toBe("ligne 2 place 2");
     expect(incrString("ligne 42 place 42")).toBe("ligne 42 place 43");
     expect(incrString("ligne 42 place 42")).toBe("ligne 42 place 43");
   });
 
-  it("Return 1 when send empty or object parameter", () => {
+  it("should return 1 when send empty or object parameter", () => {
     expect(incrString("")).toBe("1");
     expect(incrString({} as any)).toBe("1");
   });
 
-  it("Incremente number when send number", () => {
+  it("should incremente number when send number", () => {
     expect(incrString(3)).toBe(4);
     expect(incrString(41)).toBe(42);
   });
 
-  it("README examples works", () => {
+  it("should README examples works", () => {
     expect(incrString("Hello world")).toBe("Hello world 2");
     expect(incrString("Hello world 2")).toBe("Hello world 3");
     expect(incrString("Hello world 42")).toBe("Hello world 43");
@@ -66,7 +66,7 @@ describe("Test increment string", () => {
     expect(incrString("Hello world 2", "-2")).toBe("Hello world 3");
   });
 
-  describe("Backward compatibility", () => {
+  describe("it should backward compatibility", () => {
     it("1.0.0", () => {
       expect(incrString("Hello world 42")).toBe("Hello world 43");
 
